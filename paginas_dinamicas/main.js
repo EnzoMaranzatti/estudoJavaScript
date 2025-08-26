de4845847713c9d462b356f9012856f55a38a1ef
Enzo-Maranzatti <enzomaranzattisilva@gmail.com>
2025-08-25 22:15:06 -0300

    Finalizando Projeto Alura Midi

--- paginas_dinamicas/main.js
+++ paginas_dinamicas/main.js
@@ -20,13 +20,34 @@
 */
 
 function tocaSom(idSom) {
-   document.querySelector(idSom).play();
+   const elemento = document.querySelector(idSom);
+   if(elemento !== null && elemento.localName === 'audio') {
+      elemento.play()
+   } else {
+      alert("Id som inválido!");
+   }
+   
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let i = 0;
 
-while(i <= listaDeTeclas.length) {
-   listaDeTeclas[i].onclick = tocaSom;
-   i++;
+for(let i = 0; i <= listaDeTeclas.length; i++) {
+   const tecla = listaDeTeclas[i];
+   const instrumento = tecla.classList[1];
+   const idAudio = `#som_${instrumento}`;
+
+   tecla.onclick = function() {
+      tocaSom(idAudio);
+   };
+
+   // Quando uma tecla é apertada
+   tecla.onkeydown = function(evento) {
+      if(evento.code === 'Space' || evento.code === 'Enter') {
+         tecla.classList.add('ativa');
+      }
+   }
+
+   tecla.onkeyup = function() {
+      tecla.classList.remove('ativa');
+   }
 }
